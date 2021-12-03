b730c9452deac1e83b22a4a7d876652dedef41b4
Bruce J Hawkins <60618708+BHawkins62@users.noreply.github.com>
2021-12-02 21:37:41 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -7,8 +7,7 @@ read_file = open('input.txt', 'r', encoding='utf-8')
 sweep = list(map(int,read_file.read().split('\n')))
 count = 0
 for (first, second) in pairwise(sweep):
-    if second > first:
-        count += 1
+    if second > first: count += 1
 print(" Part One: " + str(count))
 
 # Part Two
@@ -16,12 +15,9 @@ print(" Part One: " + str(count))
 read_file = open('input.txt', 'r', encoding='utf-8')
 sweep = list(map(int,read_file.read().split('\n')))
 count = 0
-s_len = len(sweep)
-for num in range(0, s_len-3):
+for num in range(0, (len(sweep) - 3)):
     first = sweep[num] + sweep[num + 1] + sweep[num + 2]
     second = sweep[num + 1] + sweep[num + 2] + sweep[num + 3]
-    if second > first:
-        count += 1
-
+    if second > first: count += 1
 print(" Part Two: " + str(count))
 
